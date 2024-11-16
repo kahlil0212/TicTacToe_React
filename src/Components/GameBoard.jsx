24bc2912f06b7dce1f8aa46464c4728cd999fc7e
Kahlil <kahlil12@hotmail.com>
2024-11-16 16:49:46 -0500

    Adding click handler functionality and state to game board

--- src/Components/GameBoard.jsx
+++ src/Components/GameBoard.jsx
@@ -1,16 +1,36 @@
+import { useState } from "react";
+
 
 //Initializing game board into 2-d array
 const initialGameBoard = new Array(3).fill(null).map( (row) => row = new Array(3).fill(null));
+// const initialGameBoard = [
+//     [null, null, null],
+//     [null, null, null],
+//     [null, null, null],
+
+// ];
 
 function GameBoard(){
 
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    function playerMoveHandler(rowIdx, colIdx){
+        setGameBoard((prevGameBoard) => {
+            //When updating object or array, it is reference based and needs a copy of the prev object. Mimics immutability for data safety
+            const updatedBoard = [...prevGameBoard.map(prevRow => [...prevRow])];
+            updatedBoard[rowIdx][colIdx] = 'X';
+
+            return updatedBoard;
+        });
+    }
+
     return (
         <ol id="game-board">
-            {initialGameBoard.map((row, rowIndex) => <li key={rowIndex}>
+            {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
                 <ol>
                     {row.map((playerSymbol, colIndex) => 
                     <li key={colIndex}>
-                        <button>{playerSymbol}</button>
+                        <button onClick={() => playerMoveHandler(rowIndex, colIndex)}>{playerSymbol}</button>
                     </li>
                         )}
                 </ol>
