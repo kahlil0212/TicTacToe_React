7d8a94a8d2acbf4e73ac01c39928f461c5bfed74
Kahlil <kahlil12@hotmail.com>
2024-11-16 14:19:23 -0500

    Adding click handler for player component

--- src/Components/Player.jsx
+++ src/Components/Player.jsx
@@ -1,16 +1,30 @@
-
+import { useState } from "react";
 
 function Player({name, symbol}) {
+
+    const [isEditing, setIsEditing] = useState(false);
+
+    function isEditingHandler () {
+        setIsEditing(!isEditing);
+    };
+
+   let userInfo = (<span className="player-name">{name}</span>);
+   let editInfo = "Edit"
+
+   if(isEditing){
+    (userInfo = <input type="text" required />);
+    editInfo = "Save";
+   }
 
     return(
         <li>
               <span className="player">
-                <span className="player-name">{name}</span>
-                <span className="player-symbol">{symbol}</span>
+                    {userInfo}
+                    <span className="player-symbol">{symbol}</span>
               </span>
-              <button>Edit</button>
-            </li>
+              <button onClick={isEditingHandler} >{editInfo}</button>
+        </li>
     )
 }
 
-export default Player
+export default Player;
