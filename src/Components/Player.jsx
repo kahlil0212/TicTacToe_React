437961cdbe8f75f9ae815a30cd95805344dcfc9c
Kahlil <kahlil12@hotmail.com>
2024-11-16 15:47:27 -0500

    Updating handlers and event changes for two way binding

--- src/Components/Player.jsx
+++ src/Components/Player.jsx
@@ -1,19 +1,31 @@
 import { useState } from "react";
 
-function Player({name, symbol}) {
+function Player({initialName, symbol}) {
 
     const [isEditing, setIsEditing] = useState(false);
+    const [playerName, setPlayerName] = useState(initialName);
 
+    /* Function to handle edit button click for player component*/
     function isEditingHandler () {
-        setIsEditing(!isEditing);
+
+        //When updating state based on prevState pass it as a function and update. Best Practice. React does make call instantly to update state
+        setIsEditing((editing) => !editing);
     };
 
-   let userInfo = (<span className="player-name">{name}</span>);
-   let editInfo = "Edit"
+    //React and Javascript automatically returns and event object back and can be used to get the value from the element is called on. This case is input element for player name
+    //Two-way binding because feeding change event value back into player name 
+    function changeNameHandler(event) {
+        setPlayerName(event.target.value)
+    }
 
+    /* Default values for player component variables*/
+   let userInfo = (<span className="player-name">{playerName}</span>);
+   let buttonCaption = "Edit"
+
+   /* If button is clicked want to allow name info to be edited and object updated*/
    if(isEditing){
-    (userInfo = <input type="text" required />);
-    editInfo = "Save";
+    (userInfo = <input type="text" required onChange ={changeNameHandler} value={playerName} />);
+    buttonCaption = "Save";
    }
 
     return(
@@ -22,7 +34,7 @@ function Player({name, symbol}) {
                     {userInfo}
                     <span className="player-symbol">{symbol}</span>
               </span>
-              <button onClick={isEditingHandler} >{editInfo}</button>
+              <button onClick={isEditingHandler} >{buttonCaption}</button>
         </li>
     )
 }
