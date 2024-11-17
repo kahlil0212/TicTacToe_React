5b14c212e53df9519a2e9c7639abdd0898d553fa
Kahlil <kahlil12@hotmail.com>
2024-11-17 15:18:02 -0500

    Reducing state management for active player and reducing code redundancy

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,28 @@ import Player from "./Components/Player"
 import GameBoard from "./Components/GameBoard"
 import Log from "./Components/Log";
 
+
+function getCurrentActivePlayer(gameTurns){
+    let currentPlayer = 'X';
+    //Deriving active player from current game state
+    if(gameTurns.length > 0 && gameTurns[0].player === 'X'){
+      currentPlayer = 'O';
+    }
+
+    return currentPlayer;
+}
+
 function App() {
 
-  const [activePlayer, setActivePlayer] = useState('X');
+  //const [activePlayer, setActivePlayer] = useState('X');
   const [gameTurns, setGameTurns] = useState([]);
 
-  function handlePlayerMove(rowIndex, colIndex) {
-    setActivePlayer((currentPlayer) => currentPlayer === 'X' ? 'O' : 'X');
-    setGameTurns((prevTurns) => {
-      let currentPlayer = 'X';
+  const activePlayer = getCurrentActivePlayer(gameTurns);
 
-      if(prevTurns.length > 0 && prevTurns[0].player === 'X'){
-        currentPlayer = 'O';
-      }
+  function handlePlayerMove(rowIndex, colIndex) {
+    //setActivePlayer((currentPlayer) => currentPlayer === 'X' ? 'O' : 'X');
+    setGameTurns((prevTurns) => {
+      let currentPlayer = getCurrentActivePlayer(prevTurns);
       const updatedTurns = [
         {square: {row: rowIndex, col: colIndex}, player: currentPlayer,
           ...prevTurns}];
